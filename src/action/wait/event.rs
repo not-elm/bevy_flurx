66024c9eb091b8fa984c9dafd09f5050132025b8
not-elm <elmgameinfo@gmail.com>
2024-04-20 17:44:04 +0900

    FIX: wait::event::read and wait::event::comes
    I have been solved the issue where the events have not been consumed correctly.

--- src/action/wait/event.rs
+++ src/action/wait/event.rs
@@ -4,15 +4,15 @@
 //! - [`wait::event::read`]
 
 
-use bevy::prelude::{Event, EventReader};
+use bevy::prelude::{Event, Events, Res};
 
-use crate::prelude::seed::{ActionSeed };
+use crate::prelude::seed::ActionSeed;
 use crate::prelude::wait;
 
 /// Waits until the specified event is sent
 ///
 /// ## Examples
-/// 
+///
 /// ```no_run
 /// use bevy::app::AppExit;
 /// use bevy::prelude::*;
@@ -26,8 +26,8 @@ use crate::prelude::wait;
 pub fn comes<E>() -> ActionSeed
     where E: Event
 {
-    wait::until(|er: EventReader<E>| {
-        !er.is_empty()
+    wait::until(|events: Res<Events<E>>| {
+        0 < events.iter_current_update_events().count()
     })
 }
 
@@ -37,7 +37,7 @@ pub fn comes<E>() -> ActionSeed
 /// This is similar to [`wait::event::comes`], except that it returns the event itself.
 ///
 /// ## Examples
-/// 
+///
 /// ```no_run
 /// use bevy::app::AppExit;
 /// use bevy::prelude::*;
@@ -51,7 +51,80 @@ pub fn comes<E>() -> ActionSeed
 pub fn read<E>() -> ActionSeed<(), E>
     where E: Event + Clone
 {
-    wait::output(|mut er: EventReader<E>| {
-        er.read().next().cloned()
+    wait::output(|events: Res<Events<E>>| {
+        events.iter_current_update_events().next().cloned()
     })
+}
+
+
+#[cfg(test)]
+mod tests {
+    use bevy::app::{Startup, Update};
+    use bevy::ecs::event::ManualEventReader;
+    use bevy::prelude::{Commands, EventWriter, In};
+    use bevy_test_helper::event::{DirectEvents, TestEvent1, TestEvent2};
+
+    use crate::action::{once, wait};
+    use crate::prelude::{Either, Pipe, Reactor, Then};
+    use crate::tests::test_app;
+
+    #[test]
+    fn wait_event_consumed_events() {
+        let mut app = test_app();
+        app.add_systems(Startup, |mut commands: Commands| {
+            commands.spawn(Reactor::schedule(|task| async move {
+                task.will(Update, once::event::send_default::<TestEvent1>()
+                    .then(wait::event::comes::<TestEvent1>()),
+                ).await;
+
+                task.will(Update, {
+                    wait::either(
+                        wait::event::comes::<TestEvent1>(),
+                        once::run(|| {}),
+                    )
+                        .pipe(once::run(|In(either): In<Either<(), ()>>, mut ew: EventWriter<TestEvent2>| {
+                            if either.is_right() {
+                                ew.send_default();
+                            }
+                        }))
+                }).await;
+            }));
+        });
+
+        app.update();
+
+        app.update();
+        let mut er = ManualEventReader::<TestEvent2>::default();
+        app.assert_event_comes(&mut er);
+    }
+
+    #[test]
+    fn wait_read_event_consumed_events() {
+        let mut app = test_app();
+        app.add_systems(Startup, |mut commands: Commands| {
+            commands.spawn(Reactor::schedule(|task| async move {
+                task.will(Update, once::event::send_default::<TestEvent1>()
+                    .then(wait::event::read::<TestEvent1>()),
+                ).await;
+
+                task.will(Update, {
+                    wait::either(
+                        wait::event::read::<TestEvent1>(),
+                        once::run(|| {}),
+                    )
+                        .pipe(once::run(|In(either): In<Either<TestEvent1, ()>>, mut ew: EventWriter<TestEvent2>| {
+                            if either.is_right() {
+                                ew.send_default();
+                            }
+                        }))
+                }).await;
+            }));
+        });
+
+        app.update();
+
+        app.update();
+        let mut er = ManualEventReader::<TestEvent2>::default();
+        app.assert_event_comes(&mut er);
+    }
 }
