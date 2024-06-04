fb0bffa7356683a9bf2c66b9d985ea53af976196
elm <71685838+not-elm@users.noreply.github.com>
2024-06-04 19:47:47 +0900

    [Fix] ScheduleReactor trait (#53)
    fixed lifetime problems

--- src/extension.rs
+++ src/extension.rs
@@ -14,17 +14,17 @@ use crate::world_ptr::WorldPtr;
 ///
 /// [`World`]: bevy::prelude::World
 /// [`Commands`]: bevy::prelude::Commands
-pub trait ScheduleReactor<'w, Fun, Fut, Out> {
+pub trait ScheduleReactor<'w, Fun, Fut, Out: 'w> {
     /// Create and initialize [`Reactor`].
-    fn spawn_initialized_reactor(&'w mut self, f: Fun) -> Out;
+    fn spawn_initialized_reactor(self, f: Fun) -> Out;
 }
 
-impl<'w, Fun, Fut> ScheduleReactor<'w, Fun, Fut, EntityWorldMut<'w>> for World
+impl<'w, Fun, Fut> ScheduleReactor<'w, Fun, Fut, EntityWorldMut<'w>> for &'w mut World
     where
         Fun: FnOnce(ReactiveTask) -> Fut + 'static,
         Fut: Future + 'static
 {
-    fn spawn_initialized_reactor(&'w mut self, f: Fun) -> EntityWorldMut<'w> {
+    fn spawn_initialized_reactor(self, f: Fun) -> EntityWorldMut<'w> {
         let mut reactor = Reactor::schedule(f);
         reactor.run_sync(WorldPtr::new(self));
         reactor.initialized = true;
@@ -32,15 +32,53 @@ impl<'w, Fun, Fut> ScheduleReactor<'w, Fun, Fut, EntityWorldMut<'w>> for World
     }
 }
 
-impl<'w, 'b, F, Fut> ScheduleReactor<'w, F, Fut, ()> for Commands<'w, 'b>
+impl<'w, 'b, F, Fut> ScheduleReactor<'w, F, Fut, ()> for &mut Commands<'w, 'b>
     where
         F: FnOnce(ReactiveTask) -> Fut + Send + 'static,
         Fut: Future + 'static
 {
     #[inline]
-    fn spawn_initialized_reactor(&'w mut self, f: F) {
+    fn spawn_initialized_reactor(self, f: F) {
         self.add(|world: &mut World| {
             world.spawn_initialized_reactor(f);
         });
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use bevy::ecs::system::RunSystemOnce;
+    use bevy::prelude::{Commands, Update, World};
+    use bevy_test_helper::resource::count::Count;
+    use bevy_test_helper::resource::DirectResourceControl;
+
+    use crate::extension::ScheduleReactor;
+    use crate::tests::{increment_count, test_app};
+
+     #[test]
+    fn world_extension() {
+        let mut app = test_app();
+        app.update();
+        app.world.run_system_once(|world: &mut World| {
+            world.spawn_initialized_reactor(|task| async move {
+                task.will(Update, increment_count()).await;
+            });
+        });
+        app.update();
+        app.assert_resource_eq(Count(1));
+    }
+    
+    #[test]
+    fn commands_extension() {
+        let mut app = test_app();
+        app.update();
+        app.world.run_system_once(|mut commands: Commands| {
+            commands.spawn_initialized_reactor(|task| async move {
+                task.will(Update, increment_count()).await;
+            });
+        });
+        app.update();
+        app.assert_resource_eq(Count(1));
     }
 }
